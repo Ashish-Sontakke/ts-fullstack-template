d56e63752407697b66d13580412b0bf3d3656626
Ashish-Sontakke <ashishsontakke12@gmail.com>
2024-03-31 11:05:59 +0530

    set env variables connect client-server

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
-
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHealth(): {
+    status: string;
+  } {
+    return { status: 'ok' };
   }
 }
